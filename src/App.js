45836e3bf8d30f680a5b0d9b4f8754300f7debde
Alex Standfield <AlexStandfield@gmail.com>
2019-09-03 15:40:03 -0600

    afternoon-project-complete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react'
 import Navbar from './components/Navbar/Navbar'
 
+import './App.css'
+
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className='App'>
         <Navbar />
 
         <div id='main'>
-          Main
-          <button id='tell'><a>TELL ME MORE</a></button>
+          <h1 className='welcome'>Welcome to our Studio</h1>
+          <h1 className='meet'>It's nice to meet you</h1>
+          <button id='tell'><a href='#services'>TELL ME MORE</a></button>
         </div>
 
         <div id='services'>
           Services
+        </div>
+
+        <div id='portfolio'>
+          Portfolio
         </div>
 
         <div id='about'>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,20 +4,44 @@ import 'reset-css'
 import './Navbar.css'
 
 export default class Navbar extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+            menu: false
+        }
+    }
+
+    openMenu = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+
     render() {
         return (
             <div>
                 <nav className='navbar'>
                     <h1 className='navbar-title'><a href='main'>Start Bootstrap</a></h1>
-                    <div className='navbar-icon'></div>
+                    <div className='navbar-icon' onClick={this.openMenu}>
+                        &#9776;
+                    </div>
                     <ul className='navbar-menu'>
-                        <li><a href='services'>SERVICES</a></li>
-                        <li><a href='portfolio'>PORTFOLIO</a></li>
-                        <li><a href='about'>ABOUT</a></li>
-                        <li><a href='team'>TEAM</a></li>
-                        <li><a href='contact'>CONTACT</a></li>
+                        <li><a href='#services'>SERVICES</a></li>
+                        <li><a href='#portfolio'>PORTFOLIO</a></li>
+                        <li><a href='#about'>ABOUT</a></li>
+                        <li><a href='#team'>TEAM</a></li>
+                        <li><a href='#contact'>CONTACT</a></li>
                     </ul>
                 </nav>
+
+                <ul className={this.state.menu ? 'menu slide' : 'menu'}>
+                    <li><a href='#services'>SERVICES</a></li>
+                    <li><a href='#portfolio'>PORTFOLIO</a></li>
+                    <li><a href='#about'>ABOUT</a></li>
+                    <li><a href='#team'>TEAM</a></li>
+                    <li><a href='#contact'>CONTACT</a></li>
+                </ul>
 
             </div>
         )
